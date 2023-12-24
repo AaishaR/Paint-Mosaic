dcdc7324a37d467a655c4b659dfec4ef0cb64b05
Aaisha Rehman <aashu1471@gmail.com>
2023-12-24 02:42:28 +0000

    fixed slider buttons and added autolay on sliders

--- client/src/components/ArtworkSlider.jsx
+++ client/src/components/ArtworkSlider.jsx
@@ -4,21 +4,22 @@ import 'slick-carousel/slick/slick-theme.css';
 import Artwork from './Artwork';
 
 export default function ArtworkSlider(props) {
-    
+
     const SliderSettings = {
-        dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false
+        arrows: false,
+        autoplay: true,    
+        autoplaySpeed: 5000
     };
 
     return (
         <div className="artwork-container">
             <Slider {...SliderSettings}>
                 {props.artworks.length ? props.artworks.map((artwork, index) => (
-                    <Artwork key={index} artwork={artwork} favList={props.favList} setFavList={props.setFavList} user={props.user} slider={true} isAuthenticated={props.isAuthenticated}/>
+                    <Artwork key={index} artwork={artwork} favList={props.favList} setFavList={props.setFavList} user={props.user} slider={true} isAuthenticated={props.isAuthenticated} />
                 )) :
                     <p>There are no art pieces available yet</p>}
             </Slider>
