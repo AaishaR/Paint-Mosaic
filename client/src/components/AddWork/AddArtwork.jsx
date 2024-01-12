10a5778da2945ff67e14fee52d1c286e6848763b
Aaisha Rehman <aashu1471@gmail.com>
2024-01-12 21:05:24 +0000

    minor changes

--- client/src/components/AddWork/AddArtwork.jsx
+++ client/src/components/AddWork/AddArtwork.jsx
@@ -222,9 +222,11 @@ export default function AddArtwork() {
                     </div>
                 </form>
                 :
-                <button type="button" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                    <Link to={'/Account'}>Login to Continue</Link>
-                </button>
+                <div className="flex items-center justify-center h-screen">
+                    <button type="button" className="rounded-md bg-custom-color px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <Link className="text-white" to={'/Account'}>Login to Continue</Link>
+                    </button>
+                </div>
             }
         </div>
     )
