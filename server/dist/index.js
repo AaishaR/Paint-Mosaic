4eee543ec4d72086e5c9406171cc3f8045488590
Aaisha Rehman <aashu1471@gmail.com>
2024-01-12 03:51:08 +0000

    added: cors origin

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,20 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const express = require('express');
-// const app = express();
 const router_1 = __importDefault(require("./router"));
-// const cors = require('cors')
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./models/db"));
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200,
+};
 //For env File 
 dotenv_1.default.config({ path: './.env' });
 (0, db_1.default)();
 const app = (0, express_1.default)();
 const PORT = 3000;
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use(router_1.default);
 app.listen(PORT, () => {
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,14 @@
-
-// const express = require('express');
-// const app = express();
 import router from './router';
-// const cors = require('cors')
 
 import express, { Express, Request, Response , Application } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import db from './models/db';
+
+const corsOptions: cors.CorsOptions = {
+  origin: 'http://localhost:3001',
+  optionsSuccessStatus: 200,
+}
 
 //For env File 
 dotenv.config({ path: './.env' });
@@ -16,7 +17,7 @@ const app: Application = express();
 
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 app.listen(PORT, () => {
