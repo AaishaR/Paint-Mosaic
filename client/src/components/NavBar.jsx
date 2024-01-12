7ae5b7be43184c2533ba62e7b388943347dec536
Aaisha Rehman <aashu1471@gmail.com>
2024-01-12 20:29:34 +0000

    fixed: navbar selected button highlighted

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 import logo from '../media/mosaic.png';
 
 const navigation = [
-    { name: 'Home', to: '/', current: true },
+    { name: 'Home', to: '/', current: false },
     { name: 'Upload Work', to: '/addWork', current: false },
     { name: 'Account', to: '/Account', current: false },
-]
+] 
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -53,6 +53,7 @@ export default function Navbar() {
                                                     item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                                     'rounded-md px-3 py-2 text-sm font-medium'
                                                 )}
+                                                aria-current={item.current ? 'page' : undefined}
                                             >
                                                 {item.name}
                                             </Link>
