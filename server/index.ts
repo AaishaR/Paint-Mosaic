f9c2a25c2faf677566195e001994d279738b84c6
Aaisha Rehman <aashu1471@gmail.com>
2024-01-17 20:41:53 +0000

    trying cors

--- server/index.ts
+++ server/index.ts
@@ -5,10 +5,10 @@ import dotenv from 'dotenv';
 import db from './models/db';
 
 
-// const corsOptions: cors.CorsOptions = {
-//   origin: 'https://paint-mosaic-k37c.vercel.app/',
-//   optionsSuccessStatus: 200,
-// }
+const corsOptions: cors.CorsOptions = {
+  origin: 'https://paint-mosaic-k37c.vercel.app/',
+  optionsSuccessStatus: 200,
+}
 
 //For env File 
 dotenv.config({ path: './.env' });
@@ -19,7 +19,7 @@ const app: Application = express();
 
 const PORT = 3000;
 //corsOptions)
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 app.listen(PORT, () => {
