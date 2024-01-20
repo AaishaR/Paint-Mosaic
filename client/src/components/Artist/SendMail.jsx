608eea7a7f0efe9fd931a1f03fe02a3f664d4ee0
Aaisha Rehman <aashu1471@gmail.com>
2024-01-20 00:36:55 +0000

    add form and handle submit

--- client/src/components/Artist/SendMail.jsx
+++ client/src/components/Artist/SendMail.jsx
@@ -3,64 +3,123 @@ import React, { useState } from 'react'
 export default function SendMail() {
 
     const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        rememberMe: false,
+        subject: '',
+        yourName: '',
+        message: '',
     });
 
     const handleInputChange = (e) => {
-        const { name, value, type, checked } = e.target;
+        const { name, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
-            [name]: type === 'checkbox' ? checked : value,
+            [name]: value,
         }));
+
+        // console.log(formData)
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Add your form submission logic here using formData
+        // Add your logic here to handle the form submission with formData
         console.log('Form submitted:', formData);
     };
+
+    const isSubmitDisabled = !(
+        formData.subject &&
+        formData.yourName &&
+        formData.message
+    );
     return (
-        <div className="bg-white p-4 rounded-lg shadow-md">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">Contact Form</h2>
-            <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
-                <div className="mb-5">
-                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                        Your email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="name@flowbite.com"
-                        required
-                    />
+        <form >
+            <div className="space-y-12">
+                <div className="border-b border-gray-900/10 pb-12">
+                    <h2 className="text-base font-semibold leading-7 text-gray-900">Contact Form</h2>
+                    <p className="mt-1 text-sm leading-6 text-gray-600">
+                        Please add in your personal email address to be contacted by the artist.
+                    </p>
+
+                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div className="sm:col-span-4">
+                            <label htmlFor="sendTo" className="block text-sm font-medium leading-6 text-gray-900">
+                                Send To:
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">example@gmail.com</span>
+                                    <input
+                                        type="text"
+                                        name="sendTo"
+                                        id="sendTo"
+                                        autoComplete="email"
+                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                            <label htmlFor="subject" className="block text-sm font-medium leading-6 text-gray-900">
+                                Subject
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    type="text"
+                                    name="subject"
+                                    id="subject"
+                                    value={formData.subject}
+                                    onChange={handleInputChange}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                            <label htmlFor="yourName" className="block text-sm font-medium leading-6 text-gray-900">
+                                Your Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    type="text"
+                                    name="yourName"
+                                    id="yourName"
+                                    autoComplete="given-name"
+                                    value={formData.yourName}
+                                    onChange={handleInputChange}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-span-full">
+                            <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">
+                                Message
+                            </label>
+                            <div className="mt-2">
+                                <textarea
+                                    id="message"
+                                    name="message"
+                                    rows={3}
+                                    value={formData.message}
+                                    onChange={handleInputChange}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    // defaultValue={''}
+                                />
+                            </div>
+                            <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about your Query or leave a Comment on their work, Add in your persoanl e-mail address to be contacted by the artist directly.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="mb-5">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Your password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleInputChange}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        required
-                    />
-                </div>
+            </div>
+
+            <div className="mt-6 flex items-center justify-end gap-x-6">
                 <button
                     type="submit"
-                    className="rounded-md bg-custom-color px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    disabled={isSubmitDisabled} onClick={handleSubmit}
                 >
-                    Submit
+                    Send
                 </button>
-            </form>
-        </div>
+            </div>
+        </form>
     )
 }
